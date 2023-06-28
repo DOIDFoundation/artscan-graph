369129c94db1f08f290560c46212abe4332257af
dom <dom@gmail.com>
2023-06-28 19:02:41 +0800

    add NFT created by foundation factory; update owner after doidname transfer

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,43 +1,68 @@
-import { Address, ethereum, store, log, BigInt, dataSource } from "@graphprotocol/graph-ts";
-import { Blockchain, Contract, Owner, Token, Transaction, DOID, TokenMinterHolder, MinterHolder } from "../generated/schema";
-import { Transfer } from "../generated/Arts/ERC721";
-import { AddressChanged, NameRegistered } from "../generated/DoidRegistry/DoidRegistry";
-import { toBigDecimal } from "./utils/helpers";
-import { fetchName, fetchSymbol, fetchTokenURI } from "./utils/eip721";
+import {
+  Address,
+  ethereum,
+  store,
+  log,
+  BigInt,
+  dataSource,
+} from '@graphprotocol/graph-ts';
+import {
+  Blockchain,
+  Contract,
+  Owner,
+  Token,
+  Transaction,
+  DOID,
+  TokenMinterHolder,
+  MinterHolder,
+} from '../generated/schema';
+import { Transfer } from '../generated/Arts/ERC721';
+import {
+  NFTDropCollectionCreated,
+  NFTCollectionCreated,
+  NFTTimedEditionCollectionCreated,
+} from '../generated/Arts_FoundationFactory/NFTCollectionFactory';
+import { ERC721 } from '../generated/templates/ERC721/ERC721';
+import {
+  AddressChanged,
+  NameRegistered,
+} from '../generated/DoidRegistry/DoidRegistry';
+import { toBigDecimal } from './utils/helpers';
+import { fetchName, fetchSymbol, fetchTokenURI } from './utils/eip721';
 
 const BIG0 = BigInt.zero();
 const BIG1 = BigInt.fromI32(1);
 const COINTYPE_ETH = BigInt.fromI32(60);
 
 var KNOWN_MINTERS = new Map<string, string>();
-if (dataSource.network() == "goerli") {
+if (dataSource.network() == 'goerli') {
   // DOID
   KNOWN_MINTERS.set(
-    "0xf32950cf48c10431b27eff888d23cb31615dfcb4",
-    "0xafb2e1145f1a88ce489d22425ac84003fe50b3be"
+    '0xf32950cf48c10431b27eff888d23cb31615dfcb4',
+    '0xafb2e1145f1a88ce489d22425ac84003fe50b3be'
   );
   KNOWN_MINTERS.set(
-    "0xab4d8acb8538e7f2b81a8e0db6530bbec96678b5",
-    "0xafb2e1145f1a88ce489d22425ac84003fe50b3be"
+    '0xab4d8acb8538e7f2b81a8e0db6530bbec96678b5',
+    '0xafb2e1145f1a88ce489d22425ac84003fe50b3be'
   );
 } else {
   // DOID
   KNOWN_MINTERS.set(
-    "0xcb9302da98405ecc50b1d6d4f9671f05e143b5f7",
-    "0xf446563d6737df28d0fde28c82ce4f34e98540f3"
+    '0xcb9302da98405ecc50b1d6d4f9671f05e143b5f7',
+    '0xf446563d6737df28d0fde28c82ce4f34e98540f3'
   );
   KNOWN_MINTERS.set(
-    "0x8b2aff81fec4e7787aeeb257b5d99626651ee43f",
-    "0xf446563d6737df28d0fde28c82ce4f34e98540f3"
+    '0x8b2aff81fec4e7787aeeb257b5d99626651ee43f',
+    '0xf446563d6737df28d0fde28c82ce4f34e98540f3'
   );
   // TOKYO PUNKS
   KNOWN_MINTERS.set(
-    "0x59a498d8cb5f0028591c865c44f55e30b76c9611",
-    "0x02eb75be1e72e988de64f0088d654d8ea1081e87"
+    '0x59a498d8cb5f0028591c865c44f55e30b76c9611',
+    '0x02eb75be1e72e988de64f0088d654d8ea1081e87'
   );
 }
 
-log.info("network {} has {} known minters", [
+log.info('network {} has {} known minters', [
   dataSource.network(),
   KNOWN_MINTERS.size.toString(),
 ]);
@@ -66,10 +91,10 @@ function loadOrNewOwner(address: string, block: ethereum.Block): Owner {
 }
 
 export function handleTransfer(event: Transfer): void {
-  let blockchain = Blockchain.load("DOID");
+  let blockchain = Blockchain.load('DOID');
   if (blockchain === null) {
     // Blockchain
-    blockchain = new Blockchain("DOID");
+    blockchain = new Blockchain('DOID');
     blockchain.totalContracts = BigInt.zero();
     blockchain.totalTokens = BigInt.zero();
     blockchain.totalTransactions = BigInt.zero();
@@ -90,19 +115,24 @@ export function handleTransfer(event: Transfer): void {
     contract.save();
 
     // Blockchain
-    blockchain.totalContracts = blockchain.totalContracts.plus(BigInt.fromI32(1));
+    blockchain.totalContracts = blockchain.totalContracts.plus(
+      BigInt.fromI32(1)
+    );
     blockchain.save();
   }
 
   // Skip 721s without contract name.
-  if (contract.name === "unknown") 
-    return;
-  
-  blockchain.totalTransactions = blockchain.totalTransactions.plus(BigInt.fromI32(1));
+  if (contract.name === 'unknown') return;
+
+  blockchain.totalTransactions = blockchain.totalTransactions.plus(
+    BigInt.fromI32(1)
+  );
   blockchain.save();
-  contract.totalTransactions = contract.totalTransactions.plus(BigInt.fromI32(1));
+  contract.totalTransactions = contract.totalTransactions.plus(
+    BigInt.fromI32(1)
+  );
   contract.updatedAt = event.block.timestamp;
-  if (event.params.to.equals(Address.zero())){
+  if (event.params.to.equals(Address.zero())) {
     contract.totalTokens = contract.totalTokens.minus(BigInt.fromI32(1));
   }
   contract.save();
@@ -121,20 +151,22 @@ export function handleTransfer(event: Transfer): void {
   to.updatedAt = event.block.timestamp;
   to.save();
 
-  let token = Token.load(event.address.toHex() + "-" + event.params.tokenId.toString());
+  let token = Token.load(
+    event.address.toHex() + '-' + event.params.tokenId.toString()
+  );
   if (token === null) {
     // Token
-    token = new Token(event.address.toHex() + "-" + event.params.tokenId.toString());
+    token = new Token(
+      event.address.toHex() + '-' + event.params.tokenId.toString()
+    );
     token.contract = contract.id;
     token.tokenID = event.params.tokenId;
     token.tokenURI = fetchTokenURI(event.address, event.params.tokenId);
-    let isKnown =  knownMinter(event.address) 
-    if(isKnown == null){
-      token.minter = event.params.from.equals(Address.zero())
-        ? to.id
-        : from.id;
-    }else{
-      token.minter = isKnown!
+    let isKnown = knownMinter(event.address);
+    if (isKnown == null) {
+      token.minter = event.params.from.equals(Address.zero()) ? to.id : from.id;
+    } else {
+      token.minter = isKnown!;
     }
 
     token.owner = to.id;
@@ -166,39 +198,39 @@ export function handleTransfer(event: Transfer): void {
 
   // Update TokenHolder for old owner if not minting.
   if (!event.params.from.equals(Address.zero())) {
-    let minterHolderId = token.minter + "-" + from.id;
+    let minterHolderId = token.minter + '-' + from.id;
     let tokenMinterHolder = TokenMinterHolder.load(
-      token.id + "-" + minterHolderId
+      token.id + '-' + minterHolderId
     );
     if (tokenMinterHolder != null) {
-      log.debug("Holders: remove TokenMinterHolder {}", [tokenMinterHolder.id]);
-      store.remove("TokenMinterHolder", tokenMinterHolder.id);
+      log.debug('Holders: remove TokenMinterHolder {}', [tokenMinterHolder.id]);
+      store.remove('TokenMinterHolder', tokenMinterHolder.id);
       let minterHolder = MinterHolder.load(minterHolderId);
       if (minterHolder != null) {
         if (!minterHolder.totalTokens.equals(BIG1)) {
           minterHolder.totalTokens = minterHolder.totalTokens.minus(BIG1);
           minterHolder.save();
-          log.debug("Holders: MinterHolder {} now have {} tokens", [
+          log.debug('Holders: MinterHolder {} now have {} tokens', [
             minterHolder.id,
             minterHolder.totalTokens.toString(),
           ]);
         } else {
-          log.debug("Holders: remove MinterHolder {}", [minterHolder.id]);
-          store.remove("MinterHolder", minterHolder.id);
+          log.debug('Holders: remove MinterHolder {}', [minterHolder.id]);
+          store.remove('MinterHolder', minterHolder.id);
           let minter = Owner.load(token.minter);
           if (minter != null) {
             minter.totalTokenHolders = minter.totalTokenHolders.minus(BIG1);
             minter.save();
           } else {
             log.error(
-              "Holders: TokenMinterHolder found but Minter not found for {}",
+              'Holders: TokenMinterHolder found but Minter not found for {}',
               [tokenMinterHolder.id]
             );
           }
         }
       } else {
         log.error(
-          "Holders: TokenMinterHolder found but MinterHolder not found for {}",
+          'Holders: TokenMinterHolder found but MinterHolder not found for {}',
           [tokenMinterHolder.id]
         );
       }
@@ -207,7 +239,7 @@ export function handleTransfer(event: Transfer): void {
 
   // Update TokenHolder for new owner if not burning.
   if (!event.params.to.equals(Address.zero())) {
-    let minterHolderId = token.minter + "-" + to.id;
+    let minterHolderId = token.minter + '-' + to.id;
     let minterHolder = MinterHolder.load(minterHolderId);
     if (minterHolder === null) {
       minterHolder = new MinterHolder(minterHolderId);
@@ -220,27 +252,27 @@ export function handleTransfer(event: Transfer): void {
         minter.totalTokenHolders = minter.totalTokenHolders.plus(BIG1);
         minter.save();
       } else {
-        log.error("Holders: MinterHolder created but Minter not found for {}", [
+        log.error('Holders: MinterHolder created but Minter not found for {}', [
           minterHolder.id,
         ]);
       }
-      log.debug("Holders: create MinterHolder {}", [minterHolder.id]);
+      log.debug('Holders: create MinterHolder {}', [minterHolder.id]);
     }
     let tokenMinterHolder = TokenMinterHolder.load(
-      token.id + "-" + minterHolderId
+      token.id + '-' + minterHolderId
     );
     if (tokenMinterHolder === null) {
       tokenMinterHolder = new TokenMinterHolder(
-        token.id + "-" + minterHolderId
+        token.id + '-' + minterHolderId
       );
       tokenMinterHolder.token = token.id;
       tokenMinterHolder.minterHolder = minterHolderId;
       tokenMinterHolder.save();
       minterHolder.totalTokens = minterHolder.totalTokens.plus(BIG1);
       minterHolder.save();
-      log.debug("Holders: create TokenMinterHolder {}", [tokenMinterHolder.id]);
+      log.debug('Holders: create TokenMinterHolder {}', [tokenMinterHolder.id]);
     }
-    log.debug("Holders: MinterHolder {} now holds {} tokens", [
+    log.debug('Holders: MinterHolder {} now holds {} tokens', [
       minterHolder.id,
       minterHolder.totalTokens.toString(),
     ]);
@@ -261,23 +293,49 @@ export function handleTransfer(event: Transfer): void {
 }
 
 export function handleNameRegistered(event: NameRegistered): void {
-  let doid = new DOID(event.params.id.toHexString())
-  doid.owner = event.params.owner.toHexString()
-  doid.address = event.params.owner.toHexString()
-  doid.name = event.params.name.toString()
-  doid.coinType = BigInt.fromI32(60)
-  doid.createdAt = event.block.timestamp
-  doid.blockNumber = event.block.number
-  doid.save()
+  let doid = new DOID(event.params.id.toHexString());
+  doid.owner = event.params.owner.toHexString();
+  doid.address = event.params.owner.toHexString();
+  doid.name = event.params.name.toString();
+  doid.coinType = BigInt.fromI32(60);
+  doid.createdAt = event.block.timestamp;
+  doid.blockNumber = event.block.number;
+  doid.save();
 }
 
 export function handleAddressChanged(event: AddressChanged): void {
-  let node = event.params.node
-  let doid = DOID.load(node.toHexString())
-  if(doid == null){
-    return
+  let node = event.params.node;
+  let doid = DOID.load(node.toHexString());
+  if (doid == null) {
+    return;
   }
-  doid.address = event.params.newAddress.toHexString()
-  doid.coinType = event.params.coinType
-  doid.save()
+  doid.address = event.params.newAddress.toHexString();
+  doid.coinType = event.params.coinType;
+  doid.save();
+}
+
+export function handleDoidTransfer(event: Transfer): void {
+  if (event.params.from.equals(Address.zero())) {
+    // register case
+    return;
+  }
+  handleTransfer(event);
+}
+
+export function handlerFoundationNFTDropCollectionCreated(
+  event: NFTDropCollectionCreated
+): void {
+  ERC721.bind(event.params.collection);
+}
+
+export function handlerFoundationNFTCollectionCreated(
+  event: NFTCollectionCreated
+): void {
+  ERC721.bind(event.params.collection);
+}
+
+export function handlerFoundationNFTTimedEditionCollectionCreated(
+  event: NFTTimedEditionCollectionCreated
+): void {
+  ERC721.bind(event.params.collection);
 }
